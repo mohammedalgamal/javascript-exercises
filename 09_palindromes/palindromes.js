1eb1959e4b67201f67f54b762e98b8718f91d434
mohammedalgamal <mohammedalgamal95@gmail.com>
2022-10-22 18:53:53 +0200

    Solve palindromes problem

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
+function isEqual(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    };
 
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false;
+        }
+    };
+
+    return true;
+}
+
+const palindromes = function (str) {
+    str = str.toLowerCase()
+    str = str.split('');
+    let letters = str.filter(item => !(item === ' ' || item === ',' || item === '.' || item === '!'));
+    let rev = [...letters].reverse();
+    if (isEqual(letters, rev)) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
