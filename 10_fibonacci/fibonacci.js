63374dc64d9399a82350f4cce7e8f95dcfcebee9
mohammedalgamal <mohammedalgamal95@gmail.com>
2022-10-23 08:48:09 +0200

    Solve fibonacci problem

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = Number(num);
+    if (num < 0) {
+        return 'OOPS'
+    };
+    fib = [0, 1];
+    num--;
+    while (num >= 1) {
+        fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
+        num--;
+    };
+    return fib[fib.length - 1];
 };
 
 // Do not edit below this line
