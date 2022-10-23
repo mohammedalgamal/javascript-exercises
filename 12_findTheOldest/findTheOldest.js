f5e0760763031a76b701d7c68a00af1e15d0768d
mohammedalgamal <mohammedalgamal95@gmail.com>
2022-10-23 09:31:11 +0200

    Solve findTheOldest problem
    Note: it would be better to make a function to calculate age to
    achieve DRY principle.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,33 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const d = new Date();
+    curr =  d.getFullYear();
 
+    const old = people.reduce((oldest, person) => {
+        let age1 = 0;
+        let age2 = 0;
+        if (person['yearOfDeath'] === undefined) {
+            age1 = curr - person['yearOfBirth'];
+        }
+        else {
+            age1 = person['yearOfDeath'] - person['yearOfBirth'];
+        };
+
+        if (oldest['yearOfDeath'] === undefined) {
+            age2 = curr - oldest['yearOfBirth'];
+        }
+        else {
+            age2 = oldest['yearOfDeath'] - oldest['yearOfBirth'];
+        };
+
+        if (age1 > age2) {
+            oldest = person;
+        };
+
+        return oldest;
+
+    }, people[0]);
+
+    return old;
 };
 
 // Do not edit below this line
